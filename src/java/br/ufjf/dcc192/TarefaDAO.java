de3c9e7ef3ca166f0b0cb6bf8656871669214525
ice <ice@lab03m16.nrc.ice.ufjf.br>
2018-05-07 20:38:33 -0300

    Tratando acesso ao banco

--- src/java/br/ufjf/dcc192/TarefaDAO.java
+++ src/java/br/ufjf/dcc192/TarefaDAO.java
@@ -1,29 +1,55 @@
 package br.ufjf.dcc192;
 
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class TarefaDAO {
-    
+
+    private static Connection conexao;
     private static TarefaDAO instancia;
-    
-    public static TarefaDAO getInstance()
-    {
-        if (instancia == null)
-        {
+
+    public static TarefaDAO getInstance() {
+        if (instancia == null) {
             instancia = new TarefaDAO();
         }
         return instancia;
     }
-    
-    public List<String> listAll(){
+
+    public TarefaDAO() {
+        if (conexao == null) {
+            try {
+                conexao = DriverManager.getConnection("jdbc:derby://localhost:1527/dcc192-2018-1", "usuario", "senha");
+            } catch (SQLException ex) {
+                Logger.getLogger(TarefaDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    public List<String> listAll() {
         List<String> tarefas = new ArrayList<>();
-        tarefas.add("Comprar ovos");
-        tarefas.add("Comrpar farinha");
-        tarefas.add("Comprar açúcar");
+        try {
+
+            tarefas.add("Comprar ovos");
+            tarefas.add("Comrpar farinha");
+            tarefas.add("Comprar açúcar");
+            Statement comando = conexao.createStatement();
+            ResultSet resultado = comando.executeQuery("SELECT titulo from tarefas");
+            while (resultado.next()) {
+                tarefas.add(resultado.getString("titulo"));
+            }
+            resultado.close();
+            comando.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(TarefaDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return tarefas;
     }
-    
-    
+
 }
